e2699b24bd0a9bb27d1ab264484eed550d05783b
Eduado Gomez <egomezr@outlook.com>
2022-08-25 21:32:06 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+using Syncfusion.Licensing;
+
+namespace TranscribeMe;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private const string KEY = "NzA0MTkxQDMyMzAyZTMyMmUzMGZKeXA0LzhmYnlrQ3lnOWkwZ0I1aFlUQTd2Qm4xN08rMTFnY051Z0dTOFU9";
+    public App()
+    {
+        //Register Syncfusion license
+        SyncfusionLicenseProvider.RegisterLicense(KEY);
+    }
+
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TranscribeMe.ViewModels;
+public partial class MainWindowViewModel : ObservableObject
+{
+
+    [ObservableProperty]
+    public bool _IsVisible;
+}
