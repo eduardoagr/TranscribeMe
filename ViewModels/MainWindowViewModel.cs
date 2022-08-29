a4002618306bd7215aba6093d41d3edd79a357a1
Eduado Gomez <egomezr@outlook.com>
2022-08-30 01:48:52 +0200

    updated VM

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 using Microsoft.Toolkit.Uwp.Notifications;
 using Microsoft.Win32;
 
@@ -44,10 +48,10 @@ public class MainWindowViewModel
     public MainWindowViewModel()
     {
         CopyCommand = new Command(CopyAction);
-        AzureCommand = new Command(AzureAction);
+        AzureCommand = new Command(AzureActionAsync);
     }
 
-    private void AzureAction(object obj)
+    private async void AzureActionAsync(object obj)
     {
         var str = obj as string;
 
@@ -59,6 +63,7 @@ public class MainWindowViewModel
         switch (str)
         {
             case "Audio":
+                const string ext = ".wav";
                 var dlg = new OpenFileDialog
                 {
                     DefaultExt = ".mp3",
@@ -69,21 +74,17 @@ public class MainWindowViewModel
 
                 if (res! == true)
                 {
+                    var AudioName = Path.GetFileNameWithoutExtension(dlg.SafeFileName);
                     var projectPath = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
                     var FoderName = Path.Combine(projectPath!, "Audios");
-                    Directory.CreateDirectory(FoderName);
+                    var filePath = Path.Combine(FoderName, $"{AudioName}{ext}");
 
-                    using (var mp3 = new Mp3FileReader(dlg.FileName))
-                    {
-                        using (var ws = WaveFormatConversionStream.CreatePcmStream(mp3))
-                        {
-                            WaveFileWriter.CreateWaveFile(FoderName, ws);
-                        }
+                    using var mp3 = new Mp3FileReader(dlg.FileName);
+                    using var ws = WaveFormatConversionStream.CreatePcmStream(mp3);
+                    WaveFileWriter.CreateWaveFile(filePath, ws);
 
-                    }
+                    await ConvertToTetAsync(filePath);
                 }
-
-
 
                 break;
             case "Document":
@@ -110,6 +111,64 @@ public class MainWindowViewModel
         {
             Clipboard.SetText(Text);
         }
+    }
 
+    private async Task ConvertToTetAsync(string FilePath)
+    {
+        // Configure speech service
+        const string KEY = "3a04293bb4de464a915dc1c0bf3f4283";
+        const string Region = "westeurope";
+        var config = SpeechConfig.FromSubscription(KEY, Region);
+        MessageBox.Show("Ready to use speech service in " + config.Region);
+
+        // Configure voice
+        config.SpeechSynthesisVoiceName = "en-US-AriaNeural";
+
+        // Configure speech recognition
+
+        using var audioConfig = AudioConfig.FromWavFileInput(FilePath);
+        using var speechRecognizer = new SpeechRecognizer(config, audioConfig);
+        speechRecognizer.Recognizing += SpeechRecognizer_Recognizing;
+        speechRecognizer.Recognized += SpeechRecognizer_Recognized;
+        speechRecognizer.SessionStarted += SpeechRecognizer_SessionStarted;
+        speechRecognizer.SessionStopped += SpeechRecognizer_SessionStopped;
+
+        await speechRecognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+        do
+        {
+        } while (string.IsNullOrEmpty(Text));
+
+        await speechRecognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+
+
+
+    }
+
+    private void SpeechRecognizer_SessionStopped(object? sender, SessionEventArgs e)
+    {
+
+        Debug.WriteLine("Stopped");
+
+    }
+
+    private void SpeechRecognizer_SessionStarted(object? sender, SessionEventArgs e)
+    {
+        Debug.WriteLine("Started");
+    }
+
+    private void SpeechRecognizer_Recognized(object? sender, SpeechRecognitionEventArgs e)
+    {
+        var res = e.Result;
+        if (res.Reason == ResultReason.RecognizedSpeech)
+        {
+            Text = res.Text;
+            IsVisible = Visibility.Visible;
+        }
+    }
+
+    private void SpeechRecognizer_Recognizing(object? sender, SpeechRecognitionEventArgs e)
+    {
+        Debug.WriteLine("Recogizing: " + e.Result.Text);
     }
 }
