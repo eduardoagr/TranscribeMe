6197c8b0545bed3728bd5f97a478af32f31fa4d3
Eduado Gomez <egomezr@outlook.com>
2022-09-04 04:15:46 +0200

    Documented the Background clipboard

--- Helpers/BackgroundClipboard.cs
+++ Helpers/BackgroundClipboard.cs
@@ -3,19 +3,28 @@
 namespace Helpers;
 public static class BackgroundClipboard
 {
-    public static void SetText(string p_Text)
+    /// <summary>
+    /// This is a clipboard, that sets the text in a background thread
+    /// </summary>
+    /// <param name="c_text">Text to be set to the clipboard</param>
+    public static void SetText(string c_text)
     {
         var STAThread = new Thread(
             delegate ()
             {
                 // Use a fully qualified name for Clipboard otherwise it
                 // will end up calling itself.
-                System.Windows.Forms.Clipboard.SetText(p_Text);
+                System.Windows.Forms.Clipboard.SetText(c_text);
             });
         STAThread.SetApartmentState(ApartmentState.STA);
         STAThread.Start();
         STAThread.Join();
     }
+
+    /// <summary>
+    /// Method for returning the text 
+    /// </summary>
+    /// <returns></returns>
     public static string GetText()
     {
         var ReturnValue = string.Empty;
