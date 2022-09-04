54dd555a017edb6e7d889083f414c99544c1238f
Eduado Gomez <egomezr@outlook.com>
2022-09-04 05:18:26 +0200

    created the tile class

---
+++ Model/Tile.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Windows.Media;
+
+using MahApps.Metro.IconPacks;
+
+using MvvmHelpers.Commands;
+
+namespace Model;
+
+public class Tile
+{
+    public string? TileTitle
+    {
+        get; set;
+    }
+
+    public PackIconMaterialKind? TileIcon
+    {
+        get; set;
+    }
+
+    public Color? TileColor
+    {
+        get; set;
+    }
+    public Command? Command
+    {
+        get; set;
+    }
+    private bool _IsEnable;
+    public bool IsEnable
+    {
+        get => _IsEnable;
+        set
+        {
+            if (_IsEnable != value)
+            {
+                _IsEnable = value;
+                SetColor();
+            }
+        }
+    }
+
+    private void SetColor()
+    {
+        if (IsEnable)
+        {
+            TileColor = TileColor;
+        }
+        else
+        {
+            TileColor = Color.FromRgb(192, 192, 192);
+        }
+    }
+}
