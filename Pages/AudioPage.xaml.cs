9f216528ae415a23fb7eefe2020da7b19e97db17
Eduado Gomez <egomezr@outlook.com>
2022-10-19 23:02:31 +0200

    progress ring

--- Pages/AudioPage.xaml.cs
+++ Pages/AudioPage.xaml.cs
@@ -1,25 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+
+using TranscribeMe.ViewModel;
 
 namespace TranscribeMe.Pages {
-    /// <summary>
-    /// Interaction logic for AudioPage.xaml
-    /// </summary>
+
     public partial class AudioPage : Page {
         public AudioPage() {
             InitializeComponent();
+            DataContext = new AudioPageViewModel();
         }
     }
 }
---
+++ ViewModel/AudioPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TranscribeMe.ViewModel {
+    public class AudioPageViewModel {
+
+        public List<string>? Languages { get; set; }
+
+        public AudioPageViewModel() {
+            InitListLanguages();
+        }
+
+        private void InitListLanguages() {
+
+            Languages = new List<string>() {
+                "English",
+                "Spanish"
+            };
+        }
+    }
+}
